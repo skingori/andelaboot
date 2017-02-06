9eab6a464795113edfb0678ede000a7b727bf258
skingori <king@kings-MacBook-Pro.local>
2017-02-07 02:52:28 +0300

    work/main.py

--- work/main.py
+++ work/main.py
@@ -1,5 +1,5 @@
 def is_prime(number):
-    for number in range(1, 8):
+    for number in range(0, 8):
         for x in range(2, number):
             if number % x == 0:
                 return False
