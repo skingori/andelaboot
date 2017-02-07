69ae3dfb718e8be5f24c049a068737a5ef8ffa40
skingori <king@kings-MacBook-Pro.local>
2017-02-07 13:52:55 +0300

    save arezzo files

--- work/main.py
+++ work/main.py
@@ -1,5 +1,7 @@
 def is_prime(number):
-    for number in range(0, 8):
+    for number in range(1, 50):
+        if number <= 1:
+            return False
         for x in range(2, number):
             if number % x == 0:
                 return False
